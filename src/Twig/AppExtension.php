0fedd93ea63a9a8bc4567eccaee063f30ec54ebf
lcaquelard <i.glarf@gmail.com>
2023-01-24 10:56:28 +0100

    Project initialisation

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+// src/Twig/AppExtension.php
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('get_env', [$this, 'getEnvironmentVariable']),
+        ];
+    }
+
+    /**
+     * Return the value of the requested environment variable.
+     *
+     * @param String $varname
+     * @return String
+     */
+    public function getEnvironmentVariable(String $varname): String
+    {
+        return $_ENV[$varname];
+    }
+}
