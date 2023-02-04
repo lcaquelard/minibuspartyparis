b2cddfb191639b299694bed69404e6f2b24bff4b
lcaquelard <i.glarf@gmail.com>
2023-02-04 21:46:27 +0100

    Corrected a namespace

--- src/Classes/Pack.php
+++ src/Classes/Pack.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Class;
+namespace App\Classes;
 
 class Pack
 {
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Class\Pack;
+use App\Classes\Pack;
 
 class DefaultController extends AbstractController
 {
